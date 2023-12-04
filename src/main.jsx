85853172bcfa3a3dd7c62e433386a3a9ce4fb55e
Johan <johan.ramos.m@tecsup.edu.pe>
2023-12-04 10:54:13 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Dashboard from './pages/Dashboard.jsx';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard/>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBarSystem from "../components/NavBar"
+
+
+const Dashboard = () => {
+    return( 
+    <>
+        <NavBarSystem/>
+        <h1>Dashboard</h1>
+    </>
+    )
+}
+
+export default Dashboard;
